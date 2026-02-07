67a3a36315c839c966301451116d672ed7cc8c02
babak <123017086+flatorigin@users.noreply.github.com>
2026-02-07 14:22:21 -0500

    Fix vite config syntax

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwind()],
-  server: { port: 5173 }
-})
+  base: "/static/",
+  build: {
+    outDir: "dist",
+    assetsDir: "",
+  server: { port: 5173 },
+  },
+});
