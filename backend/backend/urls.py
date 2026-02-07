f63f6a40c7b3fea1cb6d988290f302b884a8a349
babak <123017086+flatorigin@users.noreply.github.com>
2026-02-07 10:59:06 -0500

    serve react from django

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,19 @@
 # file: backend/urls.py  (top-level, for clarity)
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import View
+from django.http import FileResponse
+import os
+
+class ReactAppView(View):
+    def get(self, request):
+        index_path = os.path.join(settings.BASE_DIR, "../frontend/dist/index.html")
+        return FileResponse(open(index_path, "rb"))
 
 urlpatterns = [
+    re_path(r"^.*$", ReactAppView.as_view()),
     path("admin/", admin.site.urls),
     path("api/auth/", include("djoser.urls")),
     path("api/auth/", include("djoser.urls.jwt")),
