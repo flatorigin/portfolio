45cf0e78a6ad94f5e2f29d17fdaab68d395f9424
babak <123017086+flatorigin@users.noreply.github.com>
2026-02-08 22:56:38 -0500

    consolelog api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+console.log("VITE_API_BASE =", import.meta.env.VITE_API_BASE);
 const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000/api", });
 
 api.interceptors.request.use((cfg) => {
