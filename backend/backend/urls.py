5fccf8469f74f7c213af8fe43f1178c7853704d7
babak <123017086+flatorigin@users.noreply.github.com>
2026-02-16 13:22:46 -0500

    pics not showing yet

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,37 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views import View
+from django.conf.urls.static import static
 from django.http import FileResponse
 from django.conf import settings
-from django.contrib.staticfiles.views import serve as serve_static
 import os
-
-
-FRONTEND_DIST = os.path.join(settings.BASE_DIR.parent, "frontend", "dist")
 
 
 class ReactAppView(View):
     def get(self, request):
-        index_path = os.path.join(FRONTEND_DIST, "index.html")
+        index_path = os.path.join(
+            settings.BASE_DIR.parent,
+            "frontend",
+            "dist",
+            "index.html"
+        )
         return FileResponse(open(index_path, "rb"))
 
 
-def serve_react_asset(request, path):
-    file_path = os.path.join(FRONTEND_DIST, path)
-
-    if os.path.exists(file_path):
-        return FileResponse(open(file_path, "rb"))
-
-    return ReactAppView().get(request)
-
-
 urlpatterns = [
+
     path("admin/", admin.site.urls),
+
     path("api/auth/", include("djoser.urls")),
     path("api/auth/", include("djoser.urls.jwt")),
     path("api/", include("accounts.urls")),
     path("api/", include("portfolio.urls")),
+]
 
-    # re_path(r"^assets/(?P<path>.*)$", serve_static),
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
     re_path(r"^.*$", ReactAppView.as_view()),
 ]
