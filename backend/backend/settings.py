d0eeef97cb79a088f432143d11da231c8f96a2b8
babak <123017086+flatorigin@users.noreply.github.com>
2026-02-16 22:41:28 -0500

    railway pass var

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,12 +72,19 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR.parent, "frontend", "dist")
 ]
 # If you want real email later:
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "babak@flatorigin.com"
-EMAIL_HOST_PASSWORD = "bimkvhlarcssnqhs"  # app password
+EMAIL_BACKEND = os.environ.get(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend",
+)
+
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "no-reply@portfolio.local")
+
+# Only required if using SMTP backend
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "1") == "1"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = "babak@flatorigin.com"
 
 AUTH_PASSWORD_VALIDATORS = []
