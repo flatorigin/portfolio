d4bb1db1b63506469ca28c89e90b734bac7bdbd5
babak <123017086+flatorigin@users.noreply.github.com>
2026-02-19 00:37:37 -0500

    Ensure MEDIA_ROOT folder exsits on boot

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 cd /app/backend
 
 echo "DATABASE_URL is: ${DATABASE_URL:-<missing>}"
+echo "MEDIA_ROOT is: ${MEDIA_ROOT:-<missing>}"
+
+# Ensure mounted volume subfolder exists
+mkdir -p "${MEDIA_ROOT:-/media/uploads}"
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
