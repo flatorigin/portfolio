8db43daa6a7094b332728bad5f59fe40fd382335
babak <123017086+flatorigin@users.noreply.github.com>
2026-03-15 15:53:47 -0400

    ordered recheck

--- backend/portfolio/serializers.py
+++ backend/portfolio/serializers.py
@@ -3,7 +3,6 @@
 # =============================================================================
 from rest_framework import serializers
 from django.utils import timezone
-from .serializers import PrivateMessageSerializer
 from accounts.serializers import ProfileSerializer
 
 from .models import (
